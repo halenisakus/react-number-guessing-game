e1a8969cd9b5168c8fd9304dcefdc9ba9ed39a31
halenisakus <halenisakus@gmail.com>
2020-11-06 00:25:56 +0300

    add generateNumber function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import GuessNum from './GuessNum';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    prev: [],
+    guessNum: [],
+    number: '',
+    hint: '',
+    startGame: false,
+  };
+  generateNumber = () => {
+    let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let random = Math.floor(Math.random() * (numbers.length - 1 - 0 + 1));
+    let secret = []; // [0]
+
+    for (let i = 0; i < 4; i++) {
+      secret.push(numbers[random]);
+      numbers.splice(numbers.indexOf(numbers[random]), 1);
+      random = Math.floor(Math.random() * (numbers.length - 1 - 0 + 1));
+    }
+
+    console.log(secret);
+
+    // [0, 1, 8, 6] gibi gelirse 0 üretmeyen random sayı al ve sıfırıncı indise ata
+    if (secret[0] === 0) {
+      let val = Math.floor(Math.random() * numbers.length - 1 + 1);
+      secret[0] = val;
+      numbers.splice(numbers.indexOf(numbers[val]), 1); //
+    }
+    return secret;
+  };
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
